cabe6d74a072b3ccc9de59a75e896d7a8ae4660d
STAKEWORLD <info@stakeworld.nl>
2022-06-28 07:17:16 +0200

    Disable blog for now

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -84,7 +84,7 @@ const config = {
             position: 'left',
             label: 'Nominate',
           },
-	 {to: '/blog', label: 'Blog', position: 'left'},
+//	 {to: '/blog', label: 'Blog', position: 'left'},
 //          {
 //            href: 'https://github.com/stakeworld',
 //            label: 'GitHub',

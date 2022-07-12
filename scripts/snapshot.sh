26228aaf4d544236ab631179ef95fdc47e233d7f
STAKEWORLD <info@stakeworld.nl>
2022-07-12 06:42:48 +0200

    Update snapshot logic and include rocksdb

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -12,57 +12,69 @@ error() {
 
 trap 'error $LINENO' ERR
 
-# Variables
-ksmdir="/home/polkadot/stakeworld-00k"
-dotdir="/home/polkadot/stakeworld-00p"
-ksmservice="stakeworld-00k"
-dotservice="stakeworld-00p"
-ksmport="9600"
-dotport="9610"
+# Setup variables
 snapshotdir="/var/www/snapshot"
-workdir="/root/stakeworld"
+datadir="/home/polkadot"
+workdir="/root/stakeworld-website"
 
-echo "`date` Starting..."
+# Snaphot targets
+targets=(stakeworld-00p stakeworld-03p stakeworld-00k stakeworld-03k)
 
-# KUSAMA
-# Get block height from prometheus metrics
-ksmblock=`wget -q localhost:$ksmport/metrics -O - | grep best | cut -d " " -f2`
-ksmdate=`date +"%a %d %b @ %H:%M"`
-systemctl stop $ksmservice
-sleep 5
-echo "snapshot of node $ksmservice at block $ksmblock on $ksmdate"
-cd $ksmdir/chains/ksmcc3
-tar cf - paritydb/full | lz4 > $snapshotdir/paritydb-ksm.lz4
-ksmsize=`du -sh $snapshotdir/paritydb-ksm.lz4 | cut -f1`
-ksmfullsize=`du -sh "$ksmdir/chains/ksmcc3/paritydb/full/" | cut -f1`
-systemctl start $ksmservice
+# Main snapshot function
+function snapshot {
+        echo "Snapshot of $i db=$db chain=$chain, port=$port"
+	# Get block height from prometheus metrics
+	blockheight=`wget -q localhost:$port/metrics -O - | grep best | cut -d " " -f2`
+	date=`date +"%a %d %b @ %H:%M"`
+	systemctl stop $i
+	sleep 5
+	cd $datadir/$1/chains/$chain
+	if [[ "$db" == "rocksdb" ]]; then
+    		dbdir="db/full"
+	elif [[ "$db" == "paritydb" ]]; then
+    		dbdir="paritydb/full"
+	fi
+	#tar cf - $dbdir | lz4 > $snapshotdir/$db-$chain.lz4
+	size=`du -sh $snapshotdir/$db-$chain.lz4 | cut -f1`
+	fullsize=`du -sh "$datadir/$1/chains/$chain/$dbdir" | cut -f1`
+	systemctl start $i
+	echo "| [direct link](http://snapshot.stakeworld.nl/$db-$chain.lz4) | $chain | $db | pruning | $blockheight | $size | $fullsize | $date |" >> $workdir/docs/validate/snapshot.mdx
+}
 
-# POLKADOT
-# Get block height from prometheus metrics
-echo "Starting node snapshot"
-dotblock=`wget -q localhost:$dotport/metrics -O - | grep best | cut -d " " -f2`
-dotdate=`date +"%a %d %b @ %H:%M"`
-systemctl stop $dotservice
-sleep 5
-echo "snapshot of node $dotservice at block $dotblock on $dotdate"
-cd $dotdir/chains/polkadot
-tar cf - paritydb/full | lz4 > $snapshotdir/paritydb-dot.lz4
-dotsize=`du -sh $snapshotdir/paritydb-dot.lz4 | cut -f1`
-dotfullsize=`du -sh "$dotdir/chains/polkadot/paritydb/full/" | cut -f1`
-systemctl start $dotservice
+echo "Starting snapshot service..."
 
-echo "Updating website"
-cd $workdir/docs/validate
-cat snapshot.mdx.header > snapshot.mdx
-cat << EOF >> snapshot.mdx
-| Database    | Format   | Pruning | Blockheight | Size       | Full         | Date     |
-| ----------- | -------- | ------- | ----------- | ---------- | ------------ | -------- |
-| Polkadot    | Paritydb | Pruned  | $dotblock   | $dotsize   | $dotfullsize | $dotdate |
-| Kusama      | Paritydb | Pruned  | $ksmblock   | $ksmsize   | $ksmfullsize | $ksmdate |
+echo "Setting website header"
+cat $workdir/docs/validate/snapshot.mdx.header > $workdir/docs/validate/snapshot.mdx
+cat << EOF >> $workdir/docs/validate/snapshot.mdx
+|  | Chain    | Database   | Pruning | Blockheight | Size       | Full         | Backup date     |
+| ------------------------| ----------- | -------- | ------- | ----------- | ---------- | ------------ | -------- |
 EOF
-cat snapshot.mdx.body >> snapshot.mdx
 
-echo "Deploying website"
-cd $workdir
-scripts/deploy.sh
-echo "Script finished"
+# Process all targets
+for i in "${targets[@]}"
+do
+	db=unknown
+	chain=unknown
+
+	if grep -q paritydb "/etc/systemd/system/$i.service"; then
+  		db="paritydb"
+	fi
+	if grep -q rocksdb "/etc/systemd/system/$i.service"; then
+  		db="rocksdb"
+	fi
+	if grep -q 'chain kusama' "/etc/systemd/system/$i.service"; then
+  		chain="ksmcc3"
+	fi
+	if grep -q 'chain polkadot' "/etc/systemd/system/$i.service"; then
+  		chain="polkadot"
+	fi
+
+	port=`cat /etc/systemd/system/$i.service | grep -o -P  'prometheus-port.{0,5}' | cut -d " " -f2`
+
+	snapshot "$i"
+done
+
+echo "Setting website body"
+cat $workdir/docs/validate/snapshot.mdx.body >> $workdir/docs/validate/snapshot.mdx
+
+echo Finished...

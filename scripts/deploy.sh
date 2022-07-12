941ac0871ec901d6d6c8c70c72b0972328d55a29
STAKEWORLD <info@stakeworld.nl>
2022-07-12 06:44:26 +0200

    Add yarn and enviroment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-GIT_USER=stakeworld USE_SSH=true CURRENT_BRANCH=master DEPLOYMENT_BRANCH=gh-pages npm run deploy
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+GIT_USER=stakeworld USE_SSH=true CURRENT_BRANCH=master DEPLOYMENT_BRANCH=gh-pages yarn deploy

6eaff2b216ba3c26bd689a159e2e31094fc52849
STAKEWORLD <info@stakeworld.nl>
2022-12-24 11:21:51 +0100

    Add snapshot static file

--- scripts/sizebackup.sh
+++ scripts/sizebackup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-git commit ../var/snapsize.ksmcc3.paritydb.archive.dat ../var/snapsize.polkadot.paritydb.archive.dat ../var/snapsize.ksmcc3.paritydb.dat ../var/snapsize.ksmcc3.rocksdb.dat ../var/snapsize.polkadot.paritydb.dat ../var/snapsize.polkadot.rocksdb.dat ../static/img/archivesize.png ../static/img/prunedsize.png ../static/img/snapsize.png -m "Automatic size backup commit"
+git commit ../var/snapsize.ksmcc3.paritydb.archive.dat ../var/snapsize.polkadot.paritydb.archive.dat ../var/snapsize.ksmcc3.paritydb.dat ../var/snapsize.ksmcc3.rocksdb.dat ../var/snapsize.polkadot.paritydb.dat ../var/snapsize.polkadot.rocksdb.dat ../static/img/archivesize.png ../static/img/prunedsize.png ../static/img/snapsize.png ../docs/snapshot.mdx -m "Automatic size backup commit"

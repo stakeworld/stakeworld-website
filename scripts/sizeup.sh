4ab74a790d3bb98a2d9742c69fb72d35c0264517
Stakeworld <info@stakeworld.io>
2023-07-16 06:34:32 +0200

    Split sizeup logic from snapshots

---
+++ scripts/sizeup.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+# STAKEWORLD 2023
+# Make size graphs of substrate nodes
+# Run this script in crontab
+
+workdir="/opt/stakeworld-website"
+source $workdir/scripts/sizeup.conf
+
+# Error handling
+error() {
+    echo "Error on line $1"
+    echo "Exiting"
+    tail -n100 $workdir/var/sizeup.log | mail -s "sizeup error" $email
+    exit 1
+}
+
+trap 'error $LINENO' ERR
+
+# STDOUT logfile
+exec 1>>$workdir/var/sizeup.log
+
+# START
+echo `date` "Starting sizeup run for $targets"
+
+# Check directories
+if [ ! -d $datadir ]
+then
+    echo "$datadir does not exist"
+    exit 1
+fi 
+
+function sizeup {
+	echo "Sizeup of $i chain=$chain, kind=$kind, port=$port, server=$server"
+	# Get block height from prometheus metrics
+	blockheight=`ssh $server "wget -q localhost:$port/metrics -O - | grep substrate_block_height | grep best | cut -d ' ' -f2"`
+	date=`date +"%a %d %b"`
+	if [[ "$db" == "rocksdb" ]]; then
+    		dbdir="$archivedir/$1/chains/$chain/db/full"
+	elif [[ "$db" == "paritydb" ]]; then
+    		dbdir="$archivedir/$1/chains/$chain/paritydb/full"
+	fi
+	humandbsize=`ssh $server "du --exclude='parachains' -sh $dbdir | cut -f1"`
+	dbsize=`ssh $server "du --exclude='parachains' -sb $dbdir | cut -f1"`
+	snapdate=`date "+%d/%m/%Y"`
+	echo "| $chain | $db | $kind | $blockheight | $humandbsize |" >> $workdir/docs/dbsize.mdx
+	echo "$snapdate,$dbsize" >> $workdir/var/snapsize.$chain.$db.$kind.dat
+	echo "Sizeup of $i fullsize=$humandbsize, height=$blockheight finished"
+}
+
+echo "Starting sizeup service..."
+
+date=`date +"%a %d %b"`
+
+echo "Setting dbsize header"
+cat $workdir/docs/dbsize.mdx.header > $workdir/docs/dbsize.mdx
+echo "Last update: $date" >> $workdir/docs/dbsize.mdx
+echo "" >> $workdir/docs/dbsize.mdx
+cat << EOF >> $workdir/docs/dbsize.mdx
+| Chain    | Database   | Format | Blockheight | Full         | 
+| ------------------------| ----------- | -------- | ------- | ---------- | 
+EOF
+
+for i in "${!targets[@]}"
+do
+	db=paritydb
+	chain=unknown
+	kind=archive
+        server=${targets[$i]}
+
+        if [[ $i =~ "rpc" ]]; then
+		kind="archive"
+	else
+		kind="pruned"
+	fi
+
+	if grep -q 'chain kusama' "/etc/systemd/system/$i.service"; then
+		chain="ksmcc3"
+	fi
+	if grep -q 'chain polkadot' "/etc/systemd/system/$i.service"; then
+		chain="polkadot"
+	fi
+	if grep -q 'chain westend' "/etc/systemd/system/$i.service"; then
+  		chain="westend2"
+	fi
+	if grep -q 'chain westmint' "/etc/systemd/system/$i.service"; then
+  		chain="westmint"
+	fi
+	if grep -q 'chain statemine' "/etc/systemd/system/$i.service"; then
+  		chain="statemine"
+	fi
+	if grep -q 'chain statemint' "/etc/systemd/system/$i.service"; then
+  		chain="statemint"
+	fi
+	if grep -q 'chain bridge-hub-kusama' "/etc/systemd/system/$i.service"; then
+  		chain="bridge-hub-kusama"
+	fi
+	if grep -q 'chain bridge-hub-polkadot' "/etc/systemd/system/$i.service"; then
+  		chain="bridge-hub-polkadot"
+	fi
+	if grep -q 'chain collectives-polkadot' "/etc/systemd/system/$i.service"; then
+  		chain="collectives-polkadot"
+	fi
+	if grep -q 'encointer-kusama.json' "/etc/systemd/system/$i.service"; then
+  		chain="encointer-kusama"
+	fi
+
+	port=`cat /etc/systemd/system/$i.service | grep -o -P  'prometheus-port.{0,5}' | cut -d " " -f2`
+
+	sizeup "$i"
+done
+
+echo "Setting dbsize body"
+cat $workdir/docs/dbsize.mdx.body >> $workdir/docs/dbsize.mdx
+
+# Change to workdir
+cd $workdir/scripts
+
+# Make graph
+echo "Making snapsize graph"
+./snapsize.sh
+
+# echo "Publishing website"
+./deploy.sh &>/dev/null
+
+echo Finished

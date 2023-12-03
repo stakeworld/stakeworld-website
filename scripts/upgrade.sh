2890f3918aca550cbc92bb416e3e16698b683c2e
Stakeworld <info@stakeworld.io>
2023-12-03 12:10:19 +0100

    Upgrade docusaurus

---
+++ scripts/upgrade.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+yarn upgrade @docusaurus/core@latest @docusaurus/plugin-client-redirects@latest @docusaurus/plugin-google-gtag@latest @docusaurus/preset-classic@latest @docusaurus/module-type-aliases@latest

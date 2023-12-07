bfadc30b28f24a1ad16b9e0080d6f06bdf41f634
Stakeworld <info@stakeworld.io>
2023-12-07 10:09:41 +0100

    Update docusaurus to 3.0.1

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-yarn upgrade @docusaurus/core@latest @docusaurus/plugin-client-redirects@latest @docusaurus/plugin-google-gtag@latest @docusaurus/preset-classic@latest @docusaurus/module-type-aliases@latest
+yarn upgrade @docusaurus/core@latest @docusaurus/plugin-client-redirects@latest @docusaurus/plugin-google-gtag@latest @docusaurus/preset-classic@latest @docusaurus/module-type-aliases@latest @docusaurus/types@latest

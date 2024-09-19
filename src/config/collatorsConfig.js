55d39075b2b1ad373c45dcfafd9ab4f517a37106
Stakeworld <info@stakeworld.io>
2024-09-19 18:57:38 +0200

    Correction

--- src/config/collatorsConfig.js
+++ src/config/collatorsConfig.js
@@ -1,17 +1,19 @@
+// src/config/collatorsConfig.js
+
 const collatorsConfig = {
   kusama: [
     'Kusama Assethub',
     'Kusama Bridgehub',
     'Kusama Coretime',
     'Kusama Encointer',
-    'Kusama People',
+    'Kusama People'
   ],
   polkadot: [
     'Polkadot Assethub',
     'Polkadot Bridgehub',
     'Polkadot Collectives',
     'Polkadot Coretime',
-    'Polkadot People',
+    'Polkadot People'
   ]
 };
 
